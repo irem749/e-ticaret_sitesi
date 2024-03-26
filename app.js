039b0c8e28bed8464c7853adee060b3ab930f446
İrem ÖZBEK <73761523+irem749@users.noreply.github.com>
2024-03-26 17:22:42 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const card = document.getElementsByClassName("card shadow");
+const ekle = document.getElementsByClassName("ekle");
+const ac = document.getElementsByClassName("btn btn-light position-relative");
+class alisveris {
+    constructor(image, title, price) {
+        this.image = image;
+        this.title = title;
+        this.price = price;
+
+    }
+}
+for (leti = 0; i < card.length; i++) {
+    console.log(card[i]);
+}
